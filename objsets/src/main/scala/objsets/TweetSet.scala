923fb2285a4ed74fc43d1f4f0e7185d06bc8f6f9
Octaki <6444830@naver.com>
2025-09-22 22:46:27 +0900

    Finally, implemented googleTwwets, appleTweets, and trending functions. But why are there not test code? Because I couldn't find erorr and I think my code is correct, so I will submit my code.

--- objsets/src/main/scala/objsets/TweetSet.scala
+++ objsets/src/main/scala/objsets/TweetSet.scala
@@ -213,14 +213,19 @@ object GoogleVsApple {
   val google = List("android", "Android", "galaxy", "Galaxy", "nexus", "Nexus")
   val apple = List("ios", "iOS", "iphone", "iPhone", "ipad", "iPad")
 
-    lazy val googleTweets: TweetSet = ???
-  lazy val appleTweets: TweetSet = ???
+  lazy val googleTweets: TweetSet =
+    TweetReader.allTweets.filter((t: Tweet) => google.exists(keyword => t.text.contains(keyword)))
+  lazy val appleTweets: TweetSet =
+    TweetReader.allTweets.filter((t: Tweet) => apple.exists(keyword => t.text.contains(keyword)))
   
   /**
    * A list of all tweets mentioning a keyword from either apple or google,
    * sorted by the number of retweets.
    */
-     lazy val trending: TweetList = ???
+     lazy val trending: TweetList = {
+       val unionofTrend = googleTweets.union(appleTweets)
+       unionofTrend.descendingByRetweet
+     }
   }
 
 object Main extends App {
