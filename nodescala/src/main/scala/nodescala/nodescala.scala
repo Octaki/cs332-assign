e88d1544a8dc3df81a98e01b43ce6e6cea55b819
Octaki <6444830@naver.com>
2025-11-10 19:35:21 +0900

    Implemented functions in nodescala by referring to the implementation of other codes.

--- nodescala/src/main/scala/nodescala/nodescala.scala
+++ nodescala/src/main/scala/nodescala/nodescala.scala
@@ -29,7 +29,15 @@ trait NodeScala {
    *  @param token        the cancellation token
    *  @param body         the response to write back
    */
-  private def respond(exchange: Exchange, token: CancellationToken, response: Response): Unit = ???
+  private def respond(exchange: Exchange, token: CancellationToken, response: Response): Unit = {
+    try {
+      for (chunk <- response if token.nonCancelled) {
+        exchange.write(chunk)
+      }
+    } finally {
+      exchange.close()
+    }
+  }
 
   /** A server:
    *  1) creates and starts an http listener
@@ -41,7 +49,25 @@ trait NodeScala {
    *  @param handler        a function mapping a request to a response
    *  @return               a subscription that can stop the server and all its asynchronous operations *entirely*
    */
-  def start(relativePath: String)(handler: Request => Response): Subscription = ???
+  def start(relativePath: String)(handler: Request => Response): Subscription = {
+    val listener = createListener(relativePath)
+    val listenerSub = listener.start()
+
+    val serverSub = Future.run() { token =>
+      Future {
+        async {
+          while (token.nonCancelled) {
+            val (req, exch) = await(listener.nextRequest())
+            async {
+              respond(exch, token, handler(req))
+            }
+          }
+        }
+      }
+    }
+
+    Subscription(listenerSub, serverSub)
+  }
 
 }
 
